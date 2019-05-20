b8ab027b9f81c4f20c8264d6543922809605e70b
rmgoetz <32755225+rmgoetz@users.noreply.github.com>
2019-05-20 14:17:23 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='wavecalc',
+      version='0.1',
+      description='A tool for EM wave interactions at interfaces',
+      url='https://github.com/rmgoetz/wavecalc',
+      author='Ryan Goetz',
+      author_email='ryan.m.goetz@gmail.com',
+      license='MIT',
+      packages=['wavecalc'],
+      zip_safe=False)
